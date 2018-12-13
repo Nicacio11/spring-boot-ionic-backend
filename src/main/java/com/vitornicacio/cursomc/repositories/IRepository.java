3e6b2e64efaab6e7230dbcb580d83e5d5e739cdc
Vitor Nicacio <vitornicacio10@hotmail.com>
2018-12-13 00:51:25 -0300

    Criando repository e service para Categoria

---
+++ src/main/java/com/vitornicacio/cursomc/repositories/IRepository.java
@@ -0,0 +1,9 @@
+/*package com.vitornicacio.cursomc.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface IRepository<T> extends JpaRepository<T, Integer> {
+}
+*/
