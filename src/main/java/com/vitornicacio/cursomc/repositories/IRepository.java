56e97e3e2dc8889c36c88b23108c90d25da11b13
Vitor Nicacio <vitornicacio10@hotmail.com>
2018-12-15 12:36:53 -0300

    criando operacao de instanciação

--- src/main/java/com/vitornicacio/cursomc/repositories/IRepository.java
+++
@@ -1,9 +0,0 @@
-/*package com.vitornicacio.cursomc.repositories;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface IRepository<T> extends JpaRepository<T, Integer> {
-}
-*/
