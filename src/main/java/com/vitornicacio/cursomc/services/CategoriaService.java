d762173cef1f38894c3b5e02fb0c8a03471bdc03
Vitor Nicacio <vitornicacio10@hotmail.com>
2021-02-08 13:10:52 -0300

    correções

--- src/main/java/com/vitornicacio/cursomc/services/CategoriaService.java
+++ src/main/java/com/vitornicacio/cursomc/services/CategoriaService.java
@@ -11,6 +11,7 @@ import org.springframework.data.domain.Sort.Direction;
 import org.springframework.stereotype.Service;
 
 import com.vitornicacio.cursomc.domain.Categoria;
+import com.vitornicacio.cursomc.domain.Cliente;
 import com.vitornicacio.cursomc.dto.CategoriaDTO;
 import com.vitornicacio.cursomc.repositories.CategoriaRepository;
 import com.vitornicacio.cursomc.services.expections.DataIntegrityException;
@@ -37,9 +38,10 @@ public class CategoriaService {
 		return repo.save(pCategoria);
 	}
 	
-	public Categoria update(Categoria pCategoria) {
-		find(pCategoria.getId());
-		return repo.save(pCategoria);
+	public Categoria update(Categoria categoria) {
+		Categoria newObj = find(categoria.getId());
+		updateData(newObj, categoria);
+		return repo.save(newObj);
 	}
 	public void delete(Integer id) throws DataIntegrityException{
 		find(id);
@@ -59,5 +61,9 @@ public class CategoriaService {
 	}
 	public Categoria fromDto(CategoriaDTO objDto) {
 		return new Categoria(objDto.getId(), objDto.getNome());
+	}
+	
+	private void updateData(Categoria newObj, Categoria obj) {
+		newObj.setNome(obj.getNome());
 	}
 }
